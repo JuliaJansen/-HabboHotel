b248607352749c2b3e702610ba63d29d6dbdfa06
MaartenBrijker <maartenbrijker@gmail.com>
2016-05-05 20:04:07 +0200

    shortened code + incl row for map_value

--- Python code/csv_reader.py
+++ Python code/csv_reader.py
@@ -10,36 +10,37 @@ import csv
 ############################# delete all below later ###########################
 houses_total = 3
 pieces_of_water = 2
+total_items = houses_total + pieces_of_water + 1
 
 # new = House(x_min, y_min, type_house)
 # new_water = Water(x_min, y_min, piece_of_water, pieces_of_water)
 
 class House(object): 
-    def __init__(self, x_min, y_min, type_house):
-        self.x_min = x_min
-        self.y_min = y_min
-        self.type_house = type_house
-    def getX_min(self):
-        return self.x_min
-    def getY_min(self):
-        return self.y_min
-    def get_type_house(self):
-        return self.type_house
+	def __init__(self, x_min, y_min, type_house):
+		self.x_min = x_min
+		self.y_min = y_min
+		self.type_house = type_house
+	def getX_min(self):
+		return self.x_min
+	def getY_min(self):
+		return self.y_min
+	def get_type_house(self):
+		return self.type_house
    
 class Water(object):
-    def __init__(self, x_min, y_min, piece_of_water, pieces_of_water):
-        self.x_min = x_min
-        self.y_min = y_min
-        self.piece_of_water = piece_of_water
-        self.pieces_of_water = pieces_of_water    
-    def getX_min(self):
-        return self.x_min
-    def getY_min(self):
-        return self.y_min
-    def getPiece_of_water(self):
-        return self.piece_of_water
-    def getPieces_of_water(self):
-        return self.pieces_of_water
+	def __init__(self, x_min, y_min, piece_of_water, pieces_of_water):
+		self.x_min = x_min
+		self.y_min = y_min
+		self.piece_of_water = piece_of_water
+		self.pieces_of_water = pieces_of_water    
+	def getX_min(self):
+		return self.x_min
+	def getY_min(self):
+		return self.y_min
+	def getPiece_of_water(self):
+		return self.piece_of_water
+	def getPieces_of_water(self):
+		return self.pieces_of_water
 
 # different type of houses
 mais = "maison"
@@ -51,44 +52,43 @@ egw = "eengezinswoning"
 
 # open csv file, define fieldnames for houses, instantiate the reader
 csvfile = open('output.csv', 'r')
-fieldnames_houses = ("x_min","y_min","type")
-reader_houses = csv.DictReader(csvfile, fieldnames_houses)
+fieldnames = ("one","two","three", "four")
+reader = csv.DictReader(csvfile, fieldnames)
 
 # lists to contain houses and water
 houses = []
 water = []
+value = []
 
-# read in houses
+# read in houses -> water -> value
 count_row = 0
-for row in reader_houses:
+for row in reader:
+	
 	if (count_row < houses_total):
-		print(row['x_min'], row['y_min'], row['type'])
-		x_min = row['x_min']
-		y_min = row['y_min']
-		type_house = row['type']
+		print(row['one'], row['two'], row['three'])
+		x_min = row['one']
+		y_min = row['two']
+		type_house = row['three']
 		new_house = House(x_min, y_min, type_house)
 		houses.append(new_house)
-	count_row += 1
-
-# open csv file again, define fieldnames for water, instantiate the reader
-csvfile = open('output.csv', 'r')
-fieldnames_water = ("x_min","y_min","piece", "pieces")
-reader_water = csv.DictReader(csvfile, fieldnames_water)
-
-# read in water
-count_row = 0
-for row in reader_water:
-	if (count_row >= houses_total):
-		print(row['x_min'], row['y_min'], row['piece'], row['pieces'])
-		x_min = row['x_min']
-		y_min = row['y_min']
-		piece_of_water = row['piece']
-		pieces_of_water = row['pieces']
+	
+	elif (count_row >= houses_total and count_row < total_items - 1):
+		print(row['one'], row['two'], row['three'], row['four'])
+		x_min = row['one']
+		y_min = row['two']
+		piece_of_water = row['three']
+		pieces_of_water = row['four']
 		new_water = Water(x_min, y_min, piece_of_water, pieces_of_water)
 		water.append(new_water)
+	
+	elif (count_row == total_items - 1):
+		map_value = row['one']
+		print(map_value)
+		value.append(map_value)
+	
 	count_row += 1
 
-fill = houses + water
+fill = houses + water + value
 
 ########################################################### 
 #### function to check if all was read in properly	   ####
@@ -108,12 +108,16 @@ for objects in fill[:houses_total]:
 
 
 # loop over each water object, and its coordinates and type.
-for objects in fill[houses_total:]:
-    XXX = objects.getX_min()
-    YYY = objects.getY_min()
-    PIECE = objects.getPiece_of_water()
-    PIECES = objects.getPieces_of_water()
-    MAIN.append([XXX, YYY, PIECE, PIECES])
+for objects in fill[houses_total:total_items - 1]:
+	XXX = objects.getX_min()
+	YYY = objects.getY_min()
+	PIECE = objects.getPiece_of_water()
+	PIECES = objects.getPieces_of_water()
+	MAIN.append([XXX, YYY, PIECE, PIECES])
+
+# append the value of this map to the end of the list
+for objects in fill[total_items-1:total_items]:
+	MAIN.append([objects])
 
 # write MAIN to csv file
 with open('output_check.csv', 'wb') as csvfile:
